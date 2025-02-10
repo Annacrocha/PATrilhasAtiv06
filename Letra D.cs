0179c6866ca95bcd605711a69021e392b98903a2
Anna Rocha <168745681+Annacrocha@users.noreply.github.com>
2025-02-10 09:23:10 -0300

    programa que solicite ao usuário para inserir um número inteiro positivo e, em seguida, exiba a soma de todos os números entre 1 e o número inserido.

--- Letra D.cs
+++ Letra D.cs
@@ -1,1 +1,26 @@
+using System;
 
+namespace lista06
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Insira um número inteiro positivo:\n");
+			int n = int.Parse(Console.ReadLine());
+			
+			int soma = 0;
+			
+			Console.WriteLine("\n");
+			
+			for (int i = 1; i <= n; i++) {
+				soma += i;
+			}
+			
+			 Console.WriteLine("A soma de todos os números entre 1 e " + n +  " é: " + soma);
+			                  
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
